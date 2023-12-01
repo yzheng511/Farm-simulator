d362b049242cf761e52109f666ee808bcc608fb2
Billy Zheng <yzheng51@student.ubc.ca>
2023-11-30 17:18:16 -0800

    Phase 4: Task 2

--- src/main/model/EventLog.java
+++ src/main/model/EventLog.java
@@ -28,8 +28,9 @@ public class EventLog implements Iterable<Event> {
      * @return instance of EventLog
      */
     public static EventLog getInstance() {
-        if (theLog == null)
+        if (theLog == null) {
             theLog = new EventLog();
+        }
 
         return theLog;
     }
